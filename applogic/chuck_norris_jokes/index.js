0be88201e87d3dcf34f67c2a0295eeb1adf2dba6
Shanky <shankar.tester901@gmail.com>
2020-01-07 09:49:38 +0530

    Example to showcase how to use Zoho Catalyst using Applogic

---
+++ applogic/chuck_norris_jokes/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express=require('express');
+const axios=require('axios');
+
+var app=express();
+
+var catalyst=require('zcatalyst-sdk-node');
+
+
+app.get('/getJokes',(req,res) => {
+
+	axios.get('https://api.icndb.com/jokes')
+		.then(jokeResp => {
+			var randomNum=Math.floor(Math.random()*20);
+			// console.log(res.data.value[randomNum].categories);
+			res.send(jokeResp.data.value[randomNum].joke)
+			console.log(jokeResp.data.value[randomNum].joke);
+		})
+		.catch(err => {
+			console.log("Error received : "+err);
+			res.send("Seems something fishy! Bad Joke!");
+		});
+
+});
+
+
+module.exports=app;
